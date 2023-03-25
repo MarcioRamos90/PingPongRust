3a4be9d13f95b1631f91e5d90eebdd53b8059fdd
Marcio Ramos <marcioalvesramos90@hotmail.com>
2023-03-25 15:25:21 -0300

    Add random start  direction movement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use rand::Rng;
+
 
 #[derive(Component)]
 enum Player {
@@ -9,13 +11,20 @@ enum Player {
 #[derive(Component)]
 struct Boll;
 
+#[derive(Component)]
+struct Acceleration{
+    x: f32,
+    y: f32,
+}
+
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(keyboard_input)
-        .add_system(boll_mov)
-        .run();
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(setup)
+    .add_system(keyboard_input)
+    .add_system(boll_mov)
+    .run();
 }
 
 fn setup(
@@ -24,6 +33,11 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn(Camera2dBundle::default());
+    
+    let mut rng = rand::thread_rng();
+
+    let x: bool = rng.gen();
+    let y: bool = rng.gen();
 
     commands.spawn((
         MaterialMesh2dBundle {
@@ -33,6 +47,10 @@ fn setup(
             ..default()
         },
         Boll,
+        Acceleration {
+            x: if x {1.} else {-1.},
+            y: if y {1.} else {-1.},
+        },
     ));
 
     commands.spawn_batch(vec![
@@ -89,9 +107,9 @@ fn keyboard_input(mut query: Query<(&mut Player, &mut Transform)>, keys: Res<Inp
 }
 
 
-fn boll_mov(mut query: Query<(&mut Boll,&mut Transform)>) {
-    for (_,  mut t) in query.iter_mut() {
-        t.translation.x += 1.;
-        t.translation.y += 1.;
+fn boll_mov(mut query: Query<(&mut Boll,&mut Transform, &mut Acceleration)>) {
+    for (_,  mut t, mut a) in query.iter_mut() {
+        t.translation.x += a.x;
+        t.translation.y += a.y;
     }
 }
